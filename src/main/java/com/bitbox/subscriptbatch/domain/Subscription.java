18dd786ceb13add9dd01fd96e63531e3a366dc28
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-09-29 15:59:52 +0900

    [FIX] - EnumType.String으로 변경

--- src/main/java/com/bitbox/subscriptbatch/domain/Subscription.java
+++ src/main/java/com/bitbox/subscriptbatch/domain/Subscription.java
@@ -32,6 +32,6 @@ public class Subscription {
     private boolean isValid;
 
     @Column(name="subscription_type", nullable = false)
-    @Enumerated(EnumType.ORDINAL)
+    @Enumerated(EnumType.STRING)
     private SubscriptionType subscriptionType;
 }
